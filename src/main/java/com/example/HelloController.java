1f61a7dbbdc4a4a49f25c9f52fe110d7e04b2cf9
RomanVlasenko <ipcreeper@gmail.com>
2016-09-01 11:30:46 +0200

    Initial commit

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by qtr-ptc-7 on 9/1/2016.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
